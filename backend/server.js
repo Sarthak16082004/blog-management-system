b0fc20ab6869f8e82e8a3cdd5d1ea5fdcb18df28
Sarthak Pathak <sarthakpathak1608@gmail.com>
2026-01-23 23:52:19 +0530

    Connect MongoDB using mongoose

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,27 @@ const cors = require('cors');
 
 const authRoutes = require('./routes/authRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
+const connectMongo = require('./config/mongo');
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', protectedRoutes);
 
+// Test route
 app.get('/', (req, res) => {
-  res.send('Backend running with JWT');
+  res.send('Backend running with MySQL + MongoDB');
 });
 
+// Connect MongoDB
+connectMongo();
+
+// Start server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
