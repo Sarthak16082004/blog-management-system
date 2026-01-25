a2d91d9949d3f935b4b86dc35a6161ef92efa8f5
Sarthak Pathak <sarthakpathak1608@gmail.com>
2026-01-25 16:08:30 +0530

    Add protected routes for authenticated-only pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Blogs from './pages/Blogs';
@@ -6,16 +7,37 @@ import SingleBlog from './pages/SingleBlog';
 import CreateBlog from './pages/CreateBlog';
 import EditBlog from './pages/EditBlog';
 
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<SingleBlog />} />
-        <Route path="/create-blog" element={<CreateBlog />} />
-        <Route path="/edit-blog/:id" element={<EditBlog />} />
+
+        {/* 🔒 PROTECTED ROUTES */}
+        <Route
+          path="/create-blog"
+          element={
+            <ProtectedRoute>
+              <CreateBlog />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/edit-blog/:id"
+          element={
+            <ProtectedRoute>
+              <EditBlog />
+            </ProtectedRoute>
+          }
+        />
+
         <Route path="*" element={<Navigate to="/blogs" />} />
       </Routes>
     </BrowserRouter>
